b632d9f2b5dbf8104f9d00f7f20e79a0df95a01e
KeafeChen <keafechen@gmail.com>
2018-11-16 16:44:32 -0500

    Update with Kernel linear method
    Update with Kernel linear method
    Also, modified the code

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ import sys
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVC
 from sklearn import metrics
 import numpy.linalg as la
 import matplotlib.pyplot as pp
 
+# global define X_train, Y_train, X_test, Y_test
+global X_train
+global Y_train
+global X_test
+global Y_test
+
+# define the feature
+feature = ['sqft_living']
+
+# define the linear kernel model
+alg = SVC(C=1.0, kernel='linear')
 
 def linreg(X,y):
     return np.dot(la.pinv(X),y)
-
 
 def kford(k, X, y):
     (n, d) = np.shape(X)
@@ -27,36 +38,13 @@ def kford(k, X, y):
         z[i] = (1.0 / len(T)) * summ
     return z
 
+def kernel(X_train, Y_train, X_test, Y_test):
+    alg.fit(X_train, Y_train)
+    Y_pred = alg.predict(X_test)
 
-if __name__ == '__main__':
-    # load the data and randomly get 500 pieces of data
-    df = pd.read_csv('./kc_house_data.csv')
-    data = df.values
-    indices = np.random.randint(0, data.shape[0], 500)
-    random_sample = data[indices]
-    random_sample_df = pd.DataFrame(random_sample, columns=list(df))
+    visualizationTesting(X_test, Y_pred)
 
-    df.to_csv("500.txt", index=False)
-
-
-    # separate train, validation and test data into 8:1:1, to make it convenient, get first 500 hundred data for now
-    df_data = df.head(500)
-    train_data, val_test_data = train_test_split(df_data, test_size=0.2)
-    val_data, test_data = train_test_split(val_test_data, test_size=0.5)
-
-    # train and test
-    feature = ['sqft_living15']
-    X_train = np.array(train_data[feature])
-    Y_train = np.array(train_data['price']).reshape(-1, 1)
-    X_test = np.array(test_data[feature])
-    Y_test = np.array(test_data['price']).reshape(-1, 1)
-
-    regressor = LinearRegression()
-    regressor.fit(X_train, Y_train)
-
-    print('Intercept: {}'.format(regressor.intercept_))
-    print('Coefficients: {}'.format(regressor.coef_))
-
+def visualizationTraining(X_train, Y_train):
     # Visualizing the training Test Results
     pp.scatter(X_train, Y_train, color='red')
     pp.plot(X_train, regressor.predict(X_train), color='blue')
@@ -64,14 +52,60 @@ if __name__ == '__main__':
     pp.xlabel("Space")
     pp.ylabel("Price")
     pp.show()
+    pp.close()
 
+def visualizationTesting(X_test, Y_test):
     # Visualizing the Test Results
-    pp.scatter(X_test, Y_test, color='red')
-    pp.plot(X_test, regressor.predict(X_test), color='blue')
+    pp.scatter(X_test, Y_test, color='blue')
+    pp.plot(X_test, regressor.predict(X_test), color='red')
     pp.title("Visualization of testing Dataset")
     pp.xlabel("Space")
     pp.ylabel("Price")
     pp.show()
+    pp.close()
+
+def loadData(filename=''):
+    df = pd.read_csv(filename)
+    data = df.values
+
+    indices = np.random.randint(0, data.shape[0], 500)
+    random_sample = data[indices]
+
+    random_sample_df = pd.DataFrame(random_sample, columns=list(df))
+    df.to_csv("500.txt", index=False)
+
+    # separate train, validation and test data into 8:1:1, to make it convenient, get first 500 hundred data for now
+    df_data = df.head(500)
+
+    train_data, val_test_data = train_test_split(df_data, test_size=0.2)
+    val_data, test_data = train_test_split(val_test_data, test_size=0.5)
+
+    return [train_data, val_data, test_data]
+if __name__ == '__main__':
+
+    # load data
+    [train_data, val_data, test_data] = loadData('kc_house_data.csv')
+    # train and test
+    X_train = np.array(train_data[feature])
+    Y_train = np.array(train_data['price']).reshape(-1, 1)
+    X_test = np.array(test_data[feature])
+    Y_test = np.array(test_data['price']).reshape(-1, 1)
+
+    # define LinearRegression
+    regressor = LinearRegression()
+
+    # fit X_train and Y_train
+    regressor.fit(X_train, Y_train)
+
+    print('Intercept: {}'.format(regressor.intercept_))
+    print('Coefficients: {}'.format(regressor.coef_))
+
+
+    # Kernel function
+    kernel(X_train, Y_train, X_test, Y_test)
+
+
+
 
 
 
