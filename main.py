2553978519c2de9f533e52c2e80391e14182f369
KeafeChen <keafechen@gmail.com>
2018-12-01 16:56:08 -0500

    update code
        # Two-fold cross validation
    
        # Hyperparameter tuning with nested cross validation

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from sklearn.svm import SVC
 from sklearn import metrics
 import numpy.linalg as la
 import matplotlib.pyplot as pp
-
+from sklearn.cross_validation import cross_val_score, cross_val_predict
+import random
 # global define X_train, Y_train, X_test, Y_test
 global X_train
 global Y_train
@@ -56,7 +57,7 @@ def visualizationTraining(X_train, Y_train):
     pp.title("Visualization of Training Dataset")
     pp.xlabel("Space")
     pp.ylabel("Price")
-    pp.show()
+    #pp.show()
     pp.close()
 
 def visualizationTesting(X_test, Y_test):
@@ -66,7 +67,7 @@ def visualizationTesting(X_test, Y_test):
     pp.title("Visualization of testing Dataset")
     pp.xlabel("Space")
     pp.ylabel("Price")
-    pp.show()
+    #pp.show()
     pp.close()
 
 def loadData(filename=''):
@@ -82,41 +83,127 @@ def loadData(filename=''):
     # separate train, validation and test data into 8:1:1, to make it convenient, get first 500 hundred data for now
     df_data = df.head(500)
 
-    train_data, val_test_data = train_test_split(df_data, test_size=0.2)
-    val_data, test_data = train_test_split(val_test_data, test_size=0.5)
+    train_data, test_data = train_test_split(df_data, test_size=0.2)
 
-    return [train_data, val_data, test_data]
+
+    return [train_data, test_data, df_data]
+
+
+def TwoCrossValidation(X, Y):
+    print("Two Cross Validation ")
+
+    sample1 = list()
+    sample2 = list()
+    n = np.shape(X)[0]
+
+    y_pred = np.zeros((n, 1))
+    for i in range(int(n / 2)):
+        sample1.append(int(i))
+        sample2.append(int(i + n / 2))
+    X_train = X[sample1]
+    Y_train = Y[sample1]
+    theta_sample1 = LinearRegression()
+    theta_sample1.fit(X_train, Y_train)
+    for i in sample2:
+        y_pred[i] = (regressor.predict(X[i][0]))
+
+
+    X_train = X[sample2]
+    Y_train = Y[sample2]
+
+    theta_sample2 = LinearRegression()
+    theta_sample2.fit(X_train, Y_train)
+    for i in sample1:
+        y_pred[i] = (regressor.predict(X[i][0]))
+
+    # visualizaing
+    pp.scatter(Y, y_pred)
+    #pp.show()
+    pp.close()
+
+def TwoCrossValidationKernel(X, Y):
+    print("Two Cross Validation for linear kernel")
+
+    sample1 = list()
+    sample2 = list()
+    n = np.shape(X)[0]
+
+    y_pred = np.zeros((n, 1))
+    for i in range(int(n / 2)):
+        sample1.append(int(i))
+        sample2.append(int(i + n / 2))
+
+    alg = SVC(C=1.0, kernel='linear')
+    alg.fit(X[sample1], Y[sample1])
+    for i in sample2:
+        y_pred[i] = (alg.predict(X[i][0]))
+    alg = SVC(C=1.0, kernel='linear')
+    alg.fit(X[sample2], Y[sample2])
+    for i in sample1:
+        y_pred[i] = (alg.predict(X[i][0]))
+    pp.scatter(Y, y_pred)
+    pp.show()
+    pp.close()
+
+def bootstrapping(B, X_subset, y_subset, C):
+    n = len(X_subset)
+    bs_err = np.zeros(B)
+    for b in range(B):
+        train_samples = list(np.random.randint(0, n, n))
+        test_samples = list(set(range(n)) - set(train_samples))
+        alg = SVC(C=C, kernel='linear')
+        alg.fit(X_subset[train_samples], y_subset[train_samples])
+        err = 0
+        for i in test_samples:
+            err = err + abs(Y_whole[i] - y_pred[i])
+        err = err / len(test_samples)
+        bs_err[b] = err
+    err_res = np.mean(bs_err)
+    return err_res
+
+
+
 if __name__ == '__main__':
 
     # load data
-    [train_data, val_data, test_data] = loadData('kc_house_data.csv')
-    # train and test
+    [train_data, test_data, df_data] = loadData('kc_house_data.csv')
+    # train, validation, and test data
     X_train = np.array(train_data[feature])
     Y_train = np.array(train_data['price']).reshape(-1, 1)
     X_test = np.array(test_data[feature])
     Y_test = np.array(test_data['price']).reshape(-1, 1)
+    X_whole = np.array(df_data[feature])
+    Y_whole = np.array(df_data['price']).reshape((-1, 1))
 
     # define LinearRegression
     regressor = LinearRegression()
 
     # fit X_train and Y_train
     regressor.fit(np.array(train_data[feature]), np.array(train_data['price']).reshape(-1, 1))
-
     print('Intercept: {}'.format(regressor.intercept_))
     print('Coefficients: {}'.format(regressor.coef_))
-
     pred = regressor.predict(test_data[feature])
     # Mean Squared Error (MSE)
     msecm1 = format(np.sqrt(metrics.mean_squared_error(Y_test, pred)), '.3f')
     # Adjusted R-squared (training)
     artrcm1 = format(adjustedR2(regressor.score(train_data[feature], train_data['price']), train_data.shape[0],
                                 len(feature)), '.3f')
-
     # visualization
     visualizationTraining(np.array(train_data[feature]), np.array(train_data['price']).reshape(-1, 1))
     visualizationTesting(np.array(test_data[feature]), pred)
 
+    # twoCrossValidation
+    TwoCrossValidation(X_whole, Y_whole)
+    '''
+    # K-fold cross validation
+    # Perform 6-fold cross validation
+    scores = cross_val_score(regressor, X_whole, Y_whole, cv=6)
+    print("Cross-validated scores: ", scores)
 
+    # Make cross validated predictions
+    predictions = cross_val_predict(regressor, X_whole, Y_whole, cv=6)
+    pp.scatter(Y_whole, predictions)
+    pp.show()'''
     # ------------------------------#
     #                               #
     # Multiple Linear Regression    #
@@ -135,24 +222,81 @@ if __name__ == '__main__':
     # Adjusted R-squared (training)
     artrcm1 = format(adjustedR2(complex_model_1.score(train_data[features1], train_data['price']), train_data.shape[0],
                                 len(features1)), '.3f')
-
-
+    # visualization for Multiple Linear Regression
+    print("visualization for Multiple Linear Regression")
+    print(np.shape(Y_whole))
+    print(np.shape(pred1))
+    pp.scatter(np.array(test_data['price']).reshape(-1, 1), pred1)
+    #pp.show()
+    pp.close()
     # ----------------------------- #
     #                               #
     # Kernel function               #
     #                               #
     # ------------------------------#
-   # kernel(X_train, Y_train, X_test, Y_test)
 
+    # Training on the whole dataset
 
+    from sklearn.svm import SVC
 
+    alg = SVC(C=1.0, kernel='linear')
+    alg.fit(X_whole, Y_whole)
+    y_pred = alg.predict(X_whole)
+    print("Linear Kernel visualization on whole dataset")
+    pp.scatter(Y_whole, y_pred)
 
+    err = 0
+    for i in range(len(X_whole)):
+        err = err + abs(Y_whole[i] - y_pred[i])
+    err = err / len(X_whole)
+    print(err)
+    #pp.show()
 
+    # Two-fold cross validation
+    print("Two Cross Validation for linear kernel")
 
+    sample1 = list()
+    sample2 = list()
+    n = np.shape(X_whole)[0]
 
+    y_pred = np.zeros((n, 1))
+    for i in range(int(n / 2)):
+        sample1.append(int(i))
+        sample2.append(int(i + n / 2))
 
+    alg = SVC(C=1.0, kernel='linear')
+    alg.fit(X_whole[sample1], Y_whole[sample1])
+    for i in sample2:
+        y_pred[i] = (alg.predict(X_whole[i][0]))
+    alg = SVC(C=1.0, kernel='linear')
+    alg.fit(X_whole[sample2], Y_whole[sample2])
+    for i in sample1:
+        y_pred[i] = (alg.predict(X_whole[i][0]))
+    pp.scatter(Y_whole, y_pred)
+    #pp.show()
+    pp.close()
 
+    # Hyperparameter tuning with nested cross validation
 
+    C_list = [10.0]
+    B = 30
+    best_err = 99999999
+    best_C = 0.0
+    y_pred = np.zeros(len(X_whole), int)
+    for C in C_list:
+        err = bootstrapping(B, X_whole[sample1], Y_whole[sample1], C)
+        print("C=", C, ", err=", err)
+        if(err < best_err):
+            best_err = err;
+            best_C = C
+    print("Best_C =", C)
 
+    alg = SVC(C=best_C, kernel='linear')
+    alg.fit(X_whole[sample2],Y_whole[sample2])
+    err = 0
+    for i in sample1:
+        y_pred[i] = (alg.predict(X_whole[i][0]))
+        err = err + abs(Y_whole[i] - y_pred[i])
 
-
+    err = err / len(sample1)
+    print(err)
